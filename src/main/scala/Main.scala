453522ce07a9ebd5f085f293091cb3aab0de742d
huongvu2312 <calm.and.chaotic@gmail.com>
2021-04-20 16:07:01 +0200

    [EX1] Add function max, min, sum

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,23 @@
 object Main extends App {
-  print("Hello World")
+  /**
+    * Maximum value of an integer array
+    * @param arr input integer array
+    * @return maximum value of arr
+    */
+  def max(arr: Array[Int]): Int = arr.reduce(_ max _)
+
+  /**
+   * Minimum value of an integer array
+   * @param arr input integer array
+   * @return minimum value of arr
+   */
+  def min(arr: Array[Int]): Int = arr.reduce(_ min _)
+
+  /**
+   * Sum of all elements in an integer array
+   * @param arr input integer array
+   * @return sum of all integer elements
+   */
+  def sum(arr: Array[Int]): Int = arr.fold(0) (_ + _)
+
 }
