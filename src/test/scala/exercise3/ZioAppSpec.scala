660bb2f0412afa872e6fd70308a34b883226f305
huongvu2312 <calm.and.chaotic@gmail.com>
2021-06-11 13:50:07 +0200

    [EX3] Update test case of optional task

--- src/test/scala/exercise3/ZioAppSpec.scala
+++ src/test/scala/exercise3/ZioAppSpec.scala
@@ -1,10 +1,10 @@
 package exercise3
 
 import exercise3.ZioApp._
-import zio.random
+import zio.{ZIO, random}
 import zio.test.DefaultRunnableSpec
 import zio.test.environment.TestRandom
-import zio.test.Assertion.equalTo
+import zio.test.Assertion.{equalTo, fails, isFailure, isWithin}
 import zio.test.assert
 
 object ZioAppSpec extends DefaultRunnableSpec {
@@ -13,17 +13,40 @@ object ZioAppSpec extends DefaultRunnableSpec {
       for {
         _ <- TestRandom.feedInts(262, 392, 349, 330, 294)
         x <- random.nextIntBounded(440)
+        queue <- whiteNoise(x)
       } yield assert(
-        whiteNoise(262)
-      )(equalTo(whiteNoise(x)))
+        queue.front()
+      )(equalTo(Some(0.1983764484234759)))
     },
+
     testM("return a white noise list based on volume") {
       for {
         _ <- TestRandom.feedDoubles(0.5, 0.6, 0.7)
         y <- random.nextDouble
+        queue <- whiteNoise(262, y)
       } yield assert(
-        List(262, 0.5)
-      )(equalTo(List(262, y)))
-    }
+        queue.front()
+      )(equalTo(Some(0.3)))
+    },
+
+    testM("queue should be between -0.5 and 0.5") {
+      for {
+        queue <- whiteNoise()
+      } yield assert(
+        queue.front().get
+      )(isWithin(-0.5, 0.5))
+    },
+
+    /*testM("testing") {
+      for {
+        result <- ZIO.fail("fail").run
+      } yield assert(result)(fails(equalTo("fail")))
+    },
+
+    testM("queue should return failure if wrong parameter is given") {
+      for {
+        queue <- whiteNoise(-50)
+      } yield assert(queue)(isFailure))
+    }*/
   )
 }
